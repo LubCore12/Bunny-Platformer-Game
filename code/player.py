66c6aa2c94dfcdaf3762383580d2e648198dd7d0
Andrew Fomin <andrfom1234567810@gmail.com>
2026-03-10 16:33:49 +0700

    Platformer logic added

--- code/player.py
+++ code/player.py
@@ -8,20 +8,25 @@ class Player(Sprite):
         super().__init__(self.image, x, y, groups)
 
         self.direction = pygame.Vector2()
+        self.gravity = 15
         self.speed = 500
+        self.on_floor = False
 
         self.collisions = collisions
 
     def input(self):
         self.key = pygame.key.get_pressed()
-
         self.direction.x = int(self.key[pygame.K_d]) - int(self.key[pygame.K_a])
-        self.direction.y = int(self.key[pygame.K_s]) - int(self.key[pygame.K_w])
+
+        if self.key[pygame.K_SPACE] and self.on_floor:
+            self.direction.y = -11
 
     def move(self, delta_time):
         self.rect.x += self.direction.x * self.speed * delta_time
         self.collision('horizontal')
-        self.rect.y += self.direction.y * self.speed * delta_time
+
+        self.direction.y += self.gravity * delta_time
+        self.rect.y += self.direction.y
         self.collision('vertical')
 
     def collision(self, direction):
@@ -33,13 +38,19 @@ class Player(Sprite):
                     if self.direction.x > 0:
                         self.rect.right = collision.rect.left
 
-                elif direction == 'vertical':
+                if direction == 'vertical':
                     if self.direction.y < 0:
                         self.rect.top = collision.rect.bottom
                     if self.direction.y > 0:
                         self.rect.bottom = collision.rect.top
+                    self.direction.y = 0
 
+    def check_floor(self):
+        bottom_rect = pygame.FRect(0, 0, self.rect.width, 2).move_to(midtop = self.rect.midbottom)
+        level_rects = [sprite.rect for sprite in self.collisions]
+        self.on_floor = True if bottom_rect.collidelist(level_rects) >= 0 else False
 
     def update(self, delta_time):
+        self.check_floor()
         self.input()
         self.move(delta_time)
